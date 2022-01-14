2c7adcc6c6bbb68b66b17ce2e8e243e01625f735
Ball Man <joshua.christensen13@gmail.com>
2022-01-14 12:12:32 -0700

    decided to do it with a while loop also

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ for(var i = 0; i <= 100; i++){
         console.log(i);
     }
 }
+
+console.log("-----------");
+
+var num = 0;
+while(num <= 100){
+    num += 1;
+    if((num % 3 === 0) && (num % 5 === 0)){
+        console.log("FizzBuzz");
+    } else if(num % 3 === 0){
+        console.log("Fizz");
+    } else if(num % 5 === 0){
+            console.log("Buzz")
+    } else {
+        console.log(num);
+    }
+}
