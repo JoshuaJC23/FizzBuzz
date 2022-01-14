e3bc4fa9783f1da6075a7f7db500740bec36f79e
Ball Man <joshua.christensen13@gmail.com>
2022-01-14 11:46:17 -0700

    added numbers evenly divisible by 5 to be replaced by Buzz

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ for(var i = 0; i <= 100; i++){
         console.log("FizzBuzz");
     } else if(i % 3 === 0){
         console.log("Fizz");
+    } else {
+        if(i % 5 === 0){
+            console.log("Buzz")
+        }
     }
 }
