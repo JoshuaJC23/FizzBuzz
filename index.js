d868906c04edcb45058f6501bfd5ee69a6fbec68
Ball Man <joshua.christensen13@gmail.com>
2022-01-14 13:32:33 -0700

    grabbed the html element and displayed a string then created a new element to attach the for loop but can't figure out how to display the words in place of the numbers

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var str = document.getElementById("header");
+str.textContent = "Hello Everyone this is the FizzBuzz exercise";
+
 
 for(var i = 0; i <= 100; i++){
+    
     if((i % 3 === 0) && (i % 5 === 0)){
         console.log("FizzBuzz");
     } else if(i % 3 === 0){
@@ -9,12 +13,16 @@ for(var i = 0; i <= 100; i++){
     } else {
         console.log(i);
     }
+
+    var newDiv = document.createElement("p");
+    newDiv.textContent = i;
+    str.appendChild(newDiv);
 }
 
 console.log("-----------");
 
 var num = 0;
-while(num <= 100){
+while(num < 100){
     num += 1;
     if((num % 3 === 0) && (num % 5 === 0)){
         console.log("FizzBuzz");
