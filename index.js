79db27e167dbcee61b27cd551bb30feff9d7897d
Ball Man <joshua.christensen13@gmail.com>
2022-01-14 11:56:25 -0700

    added at the end that if none of the numbers were evenly divisible by 3 or/and 5 to just display the number

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ for(var i = 0; i <= 100; i++){
         console.log("FizzBuzz");
     } else if(i % 3 === 0){
         console.log("Fizz");
-    } else {
-        if(i % 5 === 0){
+    } else if(i % 5 === 0){
             console.log("Buzz")
-        }
+    } else {
+        console.log(i);
     }
 }
